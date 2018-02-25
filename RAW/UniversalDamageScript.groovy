c8cd4d09e6d3d87445c6d8e89cf70dd3b6e59118
Angle <Ribbonwing@gmail.com>
2018-02-24 19:19:24 -0700

    Renamed TerrainDamageScript and added UniversalDamageScript.

---
+++ RAW/UniversalDamageScript.groovy
@@ -0,0 +1,14 @@
+
+import roguelikeengine.item.*;
+import java.util.Random;
+import roguelikeengine.largeobjects.*;
+import stat.*;
+
+
+class BrittleDamageScript implements DamageScript {
+
+    public void run(Attack a, Item i) {
+           
+    }
+
+}
